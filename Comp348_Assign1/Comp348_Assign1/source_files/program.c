a7f245c62d68d33a00942e1055bd3571c20967c6
DouceNouni <roxy.perruche@hotmail.com>
2022-02-06 23:26:20 -0500

    Question 4
    I wrote the code for question 4 in the already existing program.c file.

--- Comp348_Assign1/Comp348_Assign1/source_files/program.c
+++ Comp348_Assign1/Comp348_Assign1/source_files/program.c
@@ -1,4 +1,6 @@
-﻿#include<stdio.h>
+﻿/* Functions and Pointers: Q4. */
+
+#include<stdio.h>
 #include <stdlib.h>
 #include"aggregate.h"
 
@@ -8,11 +10,35 @@
 
 void main()
 {
-    printf("I C you.\n\n");
+    /* Functions and Pointers: Q4. */
 
-    // Tests
+    float (*aggregates[5]) (float*, int size) = { &minf, &maxf, &sum, &avg, &pseudo_avg };
+
+    float five_numbers[5] = {5, 4, 3, 2, 1};
+    float ten_numbers[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    float result;
+
+
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        result = aggregates[i](five_numbers, 5);
+        printf("Result of the current function on the array of five numbers: %.2f.\n\n", result);
+
+        result = aggregates[i](ten_numbers, 10);
+        printf("Result of the current function on the array of ten numbers: %.2f.\n\n", result);
+    }
+
+
+    WINPAUSE; // Keeps the window open after execution so we can see the results.
+}
+
+void test()
+{
+    // Basic tests of the functions.
+
     float numbers[] = { 1, 2, 3, 4, 14, 8 };
-    
+
     float min = minf(numbers, 6);
     printf("Minimum value in the array: %.2f.\n", min);
 
@@ -21,12 +47,10 @@ void main()
 
     float arraysum = sum(numbers, 6);
     printf("Sum of all the values in the array: %.2f.\n", arraysum);
-    
+
     float arrayavg = avg(numbers, 6);
     printf("Average of all the values in the array: %.2f.\n", arrayavg);
 
     float pseudoavg = pseudo_avg(numbers, 6);
     printf("Averages of the minimum and maximum values in the array: %.2f.\n\n", pseudoavg);
-
-    WINPAUSE;
 }
