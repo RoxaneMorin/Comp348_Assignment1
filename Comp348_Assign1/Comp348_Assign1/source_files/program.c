6412ff8592c3a94aa8752646656b07a3beb22337
DouceNouni <roxy.perruche@hotmail.com>
2022-02-07 21:35:44 -0500

    Question 5

--- Comp348_Assign1/Comp348_Assign1/source_files/program.c
+++ Comp348_Assign1/Comp348_Assign1/source_files/program.c
@@ -8,16 +8,11 @@
 #define WINPAUSE system("pause")
 #endif
 
-void main()
+
+void question4(float* five_numbers, float* ten_numbers)
 {
-    /* Functions and Pointers: Q4. */
-
     float (*aggregates[5]) (float*, int size) = { &minf, &maxf, &sum, &avg, &pseudo_avg };
-
-    float five_numbers[5] = {5, 4, 3, 2, 1};
-    float ten_numbers[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     float result;
-
 
     int i = 0;
     for (i = 0; i < 5; i++)
@@ -28,10 +23,46 @@ void main()
         result = aggregates[i](ten_numbers, 10);
         printf("Result of the current function on the array of ten numbers: %.2f.\n\n", result);
     }
+}
 
+
+void question5(float* five_numbers, float* ten_numbers)
+{
+    typedef float (*fivefunctions[5]) (float*, int size);
+
+    fivefunctions aggregates = { &minf, &maxf, &sum, &avg, &pseudo_avg };
+    const char* funcnames[] = { "MINF", "MAXF", "SUM", "AVG", "PSEUDO_AVG" };
+    float result;
+
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        result = aggregates[i](five_numbers, 5);
+        printf("Result the function %s on the array of five numbers: %.2f.\n\n", funcnames[i], result);
+
+        result = aggregates[i](ten_numbers, 10);
+        printf("Result of the function %s on the array of ten numbers: %.2f.\n\n", funcnames[i], result);
+    }
+}
+
+
+void main()
+{
+    /* Functions and Pointers*/
+
+    float five_numbers[5] = { 5, 4, 3, 2, 1 };
+    float ten_numbers[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+
+    printf("QUESTION 4\n\n");
+    question4(five_numbers, ten_numbers);
+
+    printf("\n\nQUESTION 5\n\n");
+    question5(five_numbers, ten_numbers);
 
     WINPAUSE; // Keeps the window open after execution so we can see the results.
 }
+
 
 void test()
 {
